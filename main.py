7422f7a4de0d423afc5f042c84a91852a9f1f6d7
JohanHades07 <johan.guzman3010@gmail.com>
2024-07-01 16:29:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("johan"'')
+print("july 1st 2024")
+print("I am signing up for Replit's 100 days of Python challenge! I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day. I'll make sure to spend some time every day coding along, for a minimum of 10 minutes a day. I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen. no excuses for not coding for not coding from the middle of a field!" )
+print("I am feeling 😊")
+print("you can follow my progress at replit.com/@johanguzman3010")
